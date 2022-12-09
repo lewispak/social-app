16937ce1cbe09057b5cbe967d5a84b857b804ac4
Lewis Pakoti <lewisnpakoti@gmail.com>
2022-12-09 16:24:24 +1300

    added protected route via react-router-dom to basic layout page/ profile pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {
   RouteProvider,
   Route,
   RouterProvider,
-  Outlet
+  Outlet,
+  Navigate
 } from 'react-router-dom';
 
 import Login from './pages/login/Login';
@@ -15,6 +16,8 @@ import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 
 function App() {
+
+  const currentUser = false;
 
   const Layout = () => {
     return(
@@ -29,10 +32,20 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="login"/>
+    }
+    return children
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>),
       children: [
         {
           path: "/",
