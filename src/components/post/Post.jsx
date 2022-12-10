40b23457fabc50e6e4c24f018f27de97c9eb29de
Lewis Pakoti <lewisnpakoti@gmail.com>
2022-12-10 23:14:39 +1300

    fixed Post Link styling

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -26,7 +26,7 @@ const Post = ({ post }) => {
           <div className="userInfo">
             <img src={post.profilePic} alt="" />
             <div className="details">
-              <Link to={`/profile/${post.userId}`} style={{textDecoration:"none"}} color="inherit">
+              <Link to={`/profile/${post.userId}`} style={{ textDecoration:"none", color: "inherit" }} >
                   <span className="name">{post.name}</span>
               </Link>
                   <span className="date">1 min ago</span>
