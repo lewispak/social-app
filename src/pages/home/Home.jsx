a0b85d0cfe87999b35793690ac417fa1fc87cd9b
Lewis Pakoti <lewisnpakoti@gmail.com>
2022-12-10 16:17:34 +1300

    added stories and posts component. Added contents and styling to stories component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,40 +1,13 @@
 import './home.scss'
 
+import Stories from '../../components/stories/Stories';
+import Posts from '../../components/posts/Posts';
+
 const Home = () => {
   return (
     <div className='home'>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
+      <Stories />
+      <Posts />
     </div>
   )
 }
