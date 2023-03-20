272295731eab24b4367e6b212f7b30a44cd54a8e
Lewis Pakoti <lewisnpakoti@gmail.com>
2023-03-21 10:06:00 +1300

    updated posted comment profile image src

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -54,7 +54,7 @@ const Comments = ({postId}) => {
       {isLoading ? "Loading..."
       : data.map(comment => (
         <div className="comment">
-          <img src={comment.profilePic} alt="" />
+          <img src={"/upload/" + comment.profilePic} alt="" />
           <div className="info">
             <span>{comment.name}</span>
             <p>{comment.desc}</p>
